e69d22cb76f1647690fc89a0b96e92c7a0a0a5be
hebertcisco <mrrabbitomg@gmail.com>
2021-07-19 17:31:51 +0000

    feat: :sparkles: add new fields

--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -4,6 +4,9 @@ export interface IStockExchange {
   yield: string;
   min2Weeks: string;
   max2Weeks: string;
+  logo: string | any;
+  name: string | any;
+  site: string | any;
 }
 export interface IResult {
   result: IStockExchange[];
--- src/utils/extractHTML.ts
+++ src/utils/extractHTML.ts
@@ -60,8 +60,28 @@ async function extractHTML(htmlContent: string) {
           .find('div')
           .find('div:nth-child(1)')
           .find('strong')
-          .text()
-      });
+          .text(),
+        logo: $('#company-section')
+          .find('div')
+          .find('div.d-block.d-md-flex.mb-5.img-lazy-group')
+          .find('div.company-brand.w-100.w-md-30.p-3.rounded.mb-3.mb-md-0.bg-lazy')
+          .attr("data-img")?.replace(/[url(+)]/g, ""),
+        name: $('#company-section')
+          .find('div')
+          .find('div.d-block.d-md-flex.mb-5.img-lazy-group')
+          .find('div.company-description.w-100.w-md-70.ml-md-5')
+          .find('h4')
+          .find('span')
+          .text(),
+        site: $('#company-section')
+          .find('div')
+          .find('div.d-block.d-md-flex.mb-5.img-lazy-group')
+          .find('div.company-description.w-100.w-md-70.ml-md-5')
+          .find('span')
+          .find('a')
+          .attr("href"),
+      })
+
       resolve({ result });
     } catch (err) {
       reject(err);
