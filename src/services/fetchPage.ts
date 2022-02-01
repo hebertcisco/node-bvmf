d4f3068d231fbabf182c3c80e90d0414433e788f
hebertcisco <hebertcisco@outlook.com>
2022-02-01 10:42:57 -0300

    refactor: :coffin: remove node-fetch and changed to axios, now we use axios

--- src/services/fetchPage.ts
+++ src/services/fetchPage.ts
@@ -1,11 +1,12 @@
 import iconv from 'iconv-lite';
-import fetch from 'node-fetch';
+import axios, { AxiosResponse } from 'axios';
 
 async function fetchPage(bvmf: string, baseUrl: string) {
   return new Promise((resolve, reject) => {
-    fetch(`${baseUrl}/${bvmf}`)
-      .then((res) => res.arrayBuffer())
-      .then((arrayBuffer) => iconv.decode(Buffer.from(arrayBuffer), 'utf-8').toString())
+    axios.get(`${baseUrl}/${bvmf}`)
+      .then((res: AxiosResponse<ArrayBuffer>) => {
+        return res.data
+      }).then((arrayBuffer) => iconv.decode(Buffer.from(arrayBuffer), 'utf-8').toString())
       .then((body) => resolve(body))
       .catch((err) => reject(err));
   });
