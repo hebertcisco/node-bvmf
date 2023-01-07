5b1612aea2e7bc961bc059e9b2c27b20871b674f
hebertcisco <hebertcisco@outlook.com>
2023-01-07 03:54:52 +0000

    fix: :white_check_mark: fix main test

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,20 +1,36 @@
+import axios from 'axios';
+import htmlLoader from '../../testing/mocks/html/html-loader';
 import stock from '../index';
+import extractHTML from '../shared/utils/extractHTML';
 
-it('stock', async () => {
-  const result = await stock({
-    bvmf: 'itsa4',
-    max: 1,
+describe('stock', () => {
+  it('stock', async () => {
+    const bvmf = 'some-bvmf';
+ 
+    const htmlContent = htmlLoader({
+      site: 'statusinvest',
+      source: 'acoes/itsa4',
+    });
+    
+    const body = await extractHTML(htmlContent);
+
+    jest.spyOn(axios, 'get').mockResolvedValue({ data: body });
+
+    const result = await stock({
+      bvmf:bvmf,
+      max: 1,
+    });
+    const { stock: stockData } = result;
+  
+    const resultStock = stockData[0];
+  
+    expect(resultStock).toHaveProperty('currentValue');
+    expect(resultStock).toHaveProperty('dailyLiquidity');
+    expect(resultStock).toHaveProperty('yield');
+    expect(resultStock).toHaveProperty('min2Weeks');
+    expect(resultStock).toHaveProperty('max2Weeks');
+    expect(resultStock).toHaveProperty('logo');
+    expect(resultStock).toHaveProperty('name');
+    expect(resultStock).toHaveProperty('site');
   });
-  const { stock: stockData } = result;
-
-  const resultStock = stockData[0];
-
-  expect(resultStock).toHaveProperty('currentValue');
-  expect(resultStock).toHaveProperty('dailyLiquidity');
-  expect(resultStock).toHaveProperty('yield');
-  expect(resultStock).toHaveProperty('min2Weeks');
-  expect(resultStock).toHaveProperty('max2Weeks');
-  expect(resultStock).toHaveProperty('logo');
-  expect(resultStock).toHaveProperty('name');
-  expect(resultStock).toHaveProperty('site');
 });
