ccaea3468ab16d9601286ddaf3e14836b5d1304e
hebertcisco <hebertcisco@outlook.com>
2023-01-25 15:00:46 +0000

    v1.0.63

--- src/shared/constants/__tests__/ua.spec.ts
+++ src/shared/constants/__tests__/ua.spec.ts
@@ -4,6 +4,8 @@ describe('userAgent', () => {
   it('should return a string that matches the expected user agent format', () => {
     const result = userAgent();
     expect(typeof result).toBe('string');
-    expect(result).toMatch(/Mozilla\/5.0 \(Windows NT 10.0; Win64; x64\) AppleWebKit\/537.36 \(KHTML, like Gecko\) Chrome\/108.0.0.0 Safari\/537.36/);
+    expect(result).toMatch(
+      /Mozilla\/5.0 \(Windows NT 10.0; Win64; x64\) AppleWebKit\/537.36 \(KHTML, like Gecko\) Chrome\/108.0.0.0 Safari\/537.36/,
+    );
   });
 });
