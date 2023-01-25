680e50cd06ff685a88cb5382ccb466d146c65dee
hebertcisco <hebertcisco@outlook.com>
2023-01-25 14:20:57 +0000

    test: :white_check_mark: add jest-cucumber

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(t|j)s$',
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec|steps))\\.(t|j)s$',
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
 };
